4b2a50fa4204a818dff9cc070b82fdc38aae8d67
Varshil Shah <varshilshah.in@gmail.com>
2023-11-26 11:17:18 +0530

    Implement multiply and division

--- src/calculator/divide.js
+++ src/calculator/divide.js
@@ -1,3 +1,11 @@
 /**
  * Division
  */
+function divide(firstNum, secondNum) {
+  if (secondNum === 0) {
+    throw new Error("Can't divide by 0");
+  }
+  return firstNum / secondNum;
+}
+
+module.exports = divide;
